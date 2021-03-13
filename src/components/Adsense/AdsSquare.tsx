069f76751972f0c20c99a5cc383a934ed726afc2
prism-cube <ocean5014dev@yahoo.co.jp>
2021-03-13 23:15:45 +0900

    dev 広告非表示

--- src/components/Adsense/AdsSquare.tsx
+++ src/components/Adsense/AdsSquare.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import GoogleAds from 'react-google-ads';
 
 const AdsDiv = styled.div`
+  text-align: center;
   margin-top: 1rem;
   margin-bottom: 1rem;
 `
@@ -9,13 +10,17 @@ const AdsDiv = styled.div`
 export default function AdsSquare() {
   return (
     <AdsDiv>
-      <GoogleAds
-        client="ca-pub-7261406872445625"
-        slot="1210854134"
-        className="adsbygoogle"
-        format="auto"
-        style={{display: 'block'}}
-      />
+      {process.env.NODE_ENV === 'production' ?
+        <GoogleAds
+          client="ca-pub-7261406872445625"
+          slot="1210854134"
+          className="adsbygoogle"
+          format="auto"
+          style={{ display: 'block' }}
+        /> :
+        <p>AdsSquare : productionモード以外では広告は表示されません</p>
+      }
+      
     </AdsDiv>
   )
 }
--- src/components/Adsense/AdsWide.tsx
+++ src/components/Adsense/AdsWide.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import GoogleAds from 'react-google-ads';
 
 const AdsDiv = styled.div`
+  text-align: center;
   margin-top: 1rem;
   margin-bottom: 1rem;
 `
@@ -9,13 +10,16 @@ const AdsDiv = styled.div`
 export default function AdsWide() {
   return (
     <AdsDiv>
-      <GoogleAds
-        client="ca-pub-7261406872445625"
-        slot="9116800288"
-        className="adsbygoogle"
-        format="auto"
-        style={{display: 'block'}}
-      />
+      {process.env.NODE_ENV === 'production' ?
+        <GoogleAds
+          client="ca-pub-7261406872445625"
+          slot="9116800288"
+          className="adsbygoogle"
+          format="auto"
+          style={{ display: 'block' }}
+        /> :
+        <p>AdsWide : productionモード以外では広告は表示されません</p>
+      }
     </AdsDiv>
   )
 }
