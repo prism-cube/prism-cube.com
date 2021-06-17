f4172184b9084790769f9d0e8ac2c82edcd02b0a
prism-cube <ocean5014dev@yahoo.co.jp>
2021-06-17 20:38:12 +0900

    Next 11 アップグレード

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,17 @@ import styled from 'styled-components'
 import Layout, { menuItems } from 'src/components/layout'
 import Link from 'next/link'
 import Image from 'next/image'
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
 import Paper from '@material-ui/core/Paper'
-import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid'
+import mainImage from '../../public/images/prism-cube.jpg'
 
 const ImageDiv = styled.div`
   position: relative;
   margin-bottom: 1rem;
 `
-const ImageImg = styled.img`
+const ImageImg = styled(Image)`
   width: 100%;
   pointer-events: none;
 `
@@ -21,11 +22,11 @@ const ImageP = styled.p`
   position: absolute;
   top: 65%;
   left: 50%;
-  -ms-transform: translate(-50%,-50%);
-  -webkit-transform: translate(-50%,-50%);
-  transform: translate(-50%,-50%);
-  margin:0;
-  padding:0;
+  -ms-transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  margin: 0;
+  padding: 0;
   font-size: 8vw;
   font-weight: bold;
 `
@@ -49,7 +50,11 @@ export default function Home() {
 
       <section>
         <ImageDiv>
-          <ImageImg src="images/prism-cube.jpg" alt="PrismCube"></ImageImg>
+          <ImageImg
+            src={mainImage}
+            alt="PrismCube"
+            placeholder="blur"
+          ></ImageImg>
           <ImageP>PrismCube</ImageP>
         </ImageDiv>
       </section>
@@ -57,7 +62,7 @@ export default function Home() {
       <Container>
         <section>
           <Grid container spacing={2}>
-            {menuItems.map(item => (
+            {menuItems.map((item) => (
               <Grid key={item.name} item xs={6}>
                 <Link href={item.href} passHref>
                   <PaperItemA>
