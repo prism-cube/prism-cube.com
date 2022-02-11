299e42f057658428bc0e860d12cb727ea23680a9
prism-cube <ocean5014dev@yahoo.co.jp>
2022-02-12 01:22:42 +0900

    Works Platformで絞り込み

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,9 +1,13 @@
+import React, {useEffect, useState} from 'react'
 import Layout from 'src/components/layout'
 import Head, { siteTitle } from 'src/components/head'
 import Image from 'next/image'
 import styled from 'styled-components'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
+import FormGroup from '@material-ui/core/FormGroup'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox, { CheckboxProps } from '@material-ui/core/Checkbox'
 import { Typography } from '@material-ui/core'
 import Tooltip from '@material-ui/core/Tooltip'
 import IconButton from '@material-ui/core/IconButton'
@@ -11,8 +15,9 @@ import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkIcon from '@material-ui/icons/Link'
 import { Appstore, Googleplay } from '@icons-pack/react-simple-icons'
 import Link from 'next/link'
+import Router, { useRouter } from 'next/router'
 import AdsSquare from 'src/components/adsense/ads-square'
-import { WorksResponse } from 'src/types/works'
+import { WorkResponse, WorksResponse } from 'src/types/works'
 import { client } from 'src/utils/api'
 
 const PaperItem = styled(Paper)`
@@ -53,8 +58,75 @@ const PlatformSpan = styled.span`
   border: 1px solid rgba(0, 0, 0, 0.54);
   color: rgba(0, 0, 0, 0.54);
 `
+const PlatformFormGroup = styled(FormGroup)`
+  display: block;
+  text-align: center;
+  padding-bottom: 0.5rem;
+`
+
+const Platform = {
+  WEB: {name: 'web', label: 'Web'},
+  IOS: {name: 'ios', label: 'App (iOS)'},
+} as const;
 
 export default function Works({ works }: { works: WorksResponse }) {
+  const router = useRouter()
+  const query = router.query
+
+  const [stateWorks, setStateWorks] = useState(works.contents)
+
+  const [statePlatform, setStatePlatform] = useState({
+    web: true,
+    ios: true,
+  })
+
+  const platformChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStatePlatform({ ...statePlatform, [event.target.name]: event.target.checked })
+    let web = statePlatform.web
+    if (event.target.name === Platform.WEB.name) web = event.target.checked
+    let ios = statePlatform.ios
+    if (event.target.name === Platform.IOS.name) ios = event.target.checked
+    
+    worksFilter(web, ios)
+
+    let query = []
+    if (web) query.push(Platform.WEB.name)
+    if (ios) query.push(Platform.IOS.name)
+
+    if (query.length > 0 && query.length < Object.values(Platform).length) {
+      Router.replace({
+        pathname: '/works',
+        query: {
+          platform: query.join(',')
+        }
+      })
+    } else {
+      Router.replace('/works')
+    }
+  }
+
+  const worksFilter = (web: boolean, ios: boolean) => {
+    let newState: WorkResponse[] = []
+    for (const work of works.contents) {
+      if ((web && work.platform.includes(Platform.WEB.label)) || 
+          (ios && work.platform.includes(Platform.IOS.label))) {
+        newState.push(work)
+      }
+    }
+    if (newState.length === 0) newState = works.contents
+    setStateWorks(newState);
+  }
+
+  useEffect(() => {
+    const {platform} = query
+    if(!router.isReady || platform == undefined) {
+      return
+    }
+    const p = platform.toString().split(',')
+    setStatePlatform({web: p.includes(Platform.WEB.name), ios: p.includes(Platform.IOS.name)})
+    worksFilter(p.includes(Platform.WEB.name), p.includes(Platform.IOS.name))
+  },[query, router])
+
   return (
     <Layout>
       <Head
@@ -63,8 +135,33 @@ export default function Works({ works }: { works: WorksResponse }) {
         url={`https://prism-cube.com/works`}
       />
 
+        <PlatformFormGroup row>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={statePlatform.web}
+                onChange={platformChange}
+                name={Platform.WEB.name}
+                color="primary"
+              />
+            }
+            label={Platform.WEB.label}
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={statePlatform.ios}
+                onChange={platformChange}
+                name={Platform.IOS.name}
+                color="primary"
+              />
+            }
+            label={Platform.IOS.label}
+          />
+        </PlatformFormGroup>
+
       <Grid container spacing={2}>
-        {works.contents.map((work) => (
+        {stateWorks.map((work) => (
           <Grid key={work.id} item xs={12} sm={6}>
             <PaperItem>
               <WorkTitle variant="h5">{work.title}</WorkTitle>
